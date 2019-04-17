612354ac4b15d5bc905155bd192feab0efbddc58
Philip Chen <philipschen@hotmail.com>
2019-04-17 13:45:28 -0400

    rebase services

--- angular/src/app/services/register.service.ts
+++ angular/src/app/services/register.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { map, catchError, tap } from 'rxjs/operators';
-import { providerModel } from './models/providerModel';
+import { providerModel } from '../models/providerModel';
 
 @Injectable({
   providedIn: 'root'
